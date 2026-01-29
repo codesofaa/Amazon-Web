277577319c9529a660ec39f037347e609b85467b
codesofaa <codesofaa@gmail.com>
2026-01-29 10:46:29 +0800

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,20 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+}
+
+
+// function to save a cart to local storage
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 export function addToCart(productId) {
     let matchingItem;
@@ -32,6 +42,8 @@ export function addToCart(productId) {
         quantity
       });
     }
+    // save to storage after adding to cart
+    saveToStorage();
   };
 
   export function removeFromCart(productId) {
@@ -46,4 +58,7 @@ export function addToCart(productId) {
     });
     
     cart = newCart;
+
+    // save to storage after removing to cart
+    saveToStorage();
   }
